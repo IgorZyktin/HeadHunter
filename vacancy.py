743eb1322acbb5f49e282413c1a7e691cb745655
Igor Zyktin <nicord@yandex.ru>
2018-11-14 22:07:44 +0300

    manager extended

--- vacancy.py
+++ vacancy.py
@@ -1,12 +1,13 @@
+# -*- coding: utf-8 -*-
 """
 
-    В этом модуле находится класс вакансии и его обработчик
+    В этом модуле находится класс вакансии и его управляющий класс
 
 """
-import operator
 import handlers
 import resp
 import hh_html
+import json
 
 
 class Vacancy:
@@ -19,150 +20,131 @@ class Vacancy:
         self.url = raw_data.get('alternate_url')
         self.name = raw_data.get('name')
 
-        self.source = raw_data
+        self.raw_data = json.dumps(raw_data, ensure_ascii=False)
 
-        self._date = handlers.HandleDate(raw_data)
-        self._salary = handlers.HandleSalary(raw_data)
-        self._location = handlers.HandlerLocation(raw_data)
-        self._employer = handlers.HandleEmployer(raw_data)
-        self._snippets = handlers.HandleSnippets(raw_data)
+        self.info = handlers.handle_info(raw_data)
+        self.date = handlers.handle_date(raw_data)
+        self.salary = handlers.handle_salary(raw_data)
+        self.location = handlers.handle_location(raw_data)
+        self.employer = handlers.handle_employer(raw_data)
 
     def __str__(self):
-        return f'{self.id} {self.salary} {self.name}'
+        return f'{self.id} {self.salary.str_salary} {self.name}'
 
     def __repr__(self):
         return '[VAC] ' + str(self.id)
 
-    @property
-    def salary(self):
-        return self._salary.salary
-
-    @property
-    def avg_salary(self):
-        return self._salary.avg_salary
-
-    @property
-    def responsibility(self):
-        return self._snippets.responsibility
-
-    @property
-    def requirement(self):
-        return self._snippets.requirement
-
-    @property
-    def date(self):
-        return self._date.date
-
-    @property
-    def employer(self):
-        return self._employer.employer
-
-    @property
-    def location(self):
-        return self._location.metro
-
 
 class VacancyManager:
     """
-    Класс-обработчик для Vacancy.
+    Класс-обработчик для Vacancy
     Организует взаимодействие остальной программы с классом вакансий
     """
-    memory = []
-    actual_ids = {}
+    def __init__(self, name, raw_vacancies):
+        """
+        Генерация множества экземпляров вакансии из json-словаря
+        """
+        self.name = name
+        self._memory = {}
+        duplicates = 0
+        for raw_vacancy in raw_vacancies:
+            duplicates += self.add_vacancy(raw_vacancy)
 
-    @staticmethod
-    def initiate(raw_data):
-        # генерация новых экземпляров вакансий из списка json-словарей
-        dupl = 0
-        for raw_vacancy in raw_data:
-            if int(raw_vacancy.get('id')) in VacancyManager.actual_ids:
-                dupl += 1
-            else:
-                VacancyManager.add_vacancy(raw_vacancy)
-        VacancyManager.memory.sort(key=operator.attrgetter('avg_salary'))
-        resp.save(f'Генерация вакансий, {len(raw_data)} шт. ({dupl} повт.)', level=3)
+        text = f'Инициация "{self.name}", {len(raw_vacancies)} вакансий ({duplicates} повт.)'
+        resp.save(text, level=3)
 
-    @staticmethod
-    def add_vacancy(raw_vacancy):
-        # генерация одного нового экземпляра вакансии из json-словаря
-        if int(raw_vacancy.get('id')) not in VacancyManager.actual_ids:
-            new_vacancy = Vacancy(raw_vacancy)
-            VacancyManager.actual_ids.update({new_vacancy.id: new_vacancy})
-            VacancyManager.memory.append(new_vacancy)
+    def add_vacancy(self, raw_vacancy):
+        """
+        Генерация нового экземпляра вакансии из json-словаря
+        """
+        current_id = int(raw_vacancy.get('id'))
+        if current_id not in self._memory:
+            self._memory[current_id] = Vacancy(raw_vacancy)
+            return 0
+        return 1
 
-    @staticmethod
-    def del_vacancy_by_id(vacancy_id):
-        # удаление вакансии
-        VacancyManager.memory.remove(VacancyManager.actual_ids[vacancy_id])
-        del VacancyManager.actual_ids[vacancy_id]
+    def del_vacancy(self, target_id):
+        """
+        Удаление вакансии
+        """
+        success = self._memory.pop(target_id, False)
+        if not success:
+            text = f'Попытка удалить несуществующую вакансию {target_id} у менеджера "{self.name}""'
+            resp.save(text, level=3)
 
-    @staticmethod
-    def get_vacancy_by_index(number):
-        # получение вакансии из списка существующщих вакансий по индексу
-        if 0 <= number <= len(VacancyManager.memory):
-            return VacancyManager.memory[number]
-        raise IndexError(f'Нет вакансии с таким индексом: {number}')
+    def has_vacancy(self, target_id):
+        """
+        Проверка наличия вакансии
+        """
+        if target_id in self._memory:
+            return True
+        return False
 
-    @staticmethod
-    def get_vacancy_by_id(number):
-        # получение вакансии из списка существующщих вакансий по id
-        if number in VacancyManager.actual_ids:
-            return VacancyManager.actual_ids[number]
-        raise IndexError(f'Нет вакансии с таким id: {number}')
+    def get_vacancy(self, target_id):
+        """
+        Получение вакансии
+        """
+        if VacancyManager.has_vacancy(self, target_id):
+            return self._memory[target_id]
+        else:
+            txt = f'Попытка запросить несуществующую вакансию {target_id} у менеджера "{self.name}"'
+            resp.save(txt, level=3)
 
-    @staticmethod
-    def show_all():
-        total = len(VacancyManager.memory)
+    def demonstrate(self):
+        """
+        Вывод всех вакансий на экран
+        """
+        total = len(self._memory)
         digits = len(str(total))
-        resp.save(f'Вывод вакансий на экран {total}', level=2)
+        resp.save(f'Вывод вакансий "{self.name}" на экран в кол-ве {total} шт.', level=2)
         print(f'В памяти находится {total} записей:')
-        for i, each in enumerate(VacancyManager.memory, start=1):
+        for i, each in enumerate(self._memory, start=1):
             num = str(i).rjust(digits, '0')
             print(f'\t{num}. {each}')
 
-    @staticmethod
-    def clear():
-        resp.save(f'Очистка списка вакансий {len(VacancyManager.memory)}', level=3)
-        for i in range(len(VacancyManager.memory)):
-            del VacancyManager.memory[0]
-        VacancyManager.memory.clear()
-        VacancyManager.actual_ids.clear()
+    def erase_memory(self):
+        """
+        Полная очистка памяти менеджера
+        """
+        resp.save(f'Очистка памяти "{self.name}" в количестве {len(self._memory)} шт.', level=3)
+        self._memory.clear()
 
-    @staticmethod
-    def purge_with_words(words, field):
-        resp.save(f'Удаление вакансий с ключевыми словами "{words}" в поле "{field}" ', level=3)
+    def purge_with_words(self, words):
+        """
+        Удалить все вакансии, в описании которых есть данные слова
+        """
+        text = f'Удаление по наличию ключевых слов "{words}" менеджера "{self.name}"'
+        resp.save(text, level=2)
         for word in words:
-            if field == 'requirement':
-                for each in VacancyManager.memory:
-                    if each.requirement.find(word) != -1:
-                        VacancyManager.del_vacancy_by_id(each.id)
+            for each in self._memory.values():
+                if each.info.description is not None and word in each.info.description:
+                    self.del_vacancy(each.id)
 
-            elif field == 'responsibility':
-                for each in VacancyManager.memory:
-                    if each.responsibility.find(word) != -1:
-                        VacancyManager.del_vacancy_by_id(each.id)
+                if each.info.short is not None and word in each.info.short:
+                    self.del_vacancy(each.id)
 
-    @staticmethod
-    def purge_without_words(words, field):
-        resp.save(f'Удаление вакансий без ключевых слов: {words} - {field} ', level=3)
+    def purge_without_words(self, words):
+        """
+        Удалить все вакансии, в описании которых нет данных слов
+        """
+        text = f'Удаление по отсутствию ключевых слов "{words}" менеджера "{self.name}"'
+        resp.save(text, level=2)
         for word in words:
-            if field == 'requirement':
-                for each in VacancyManager.memory:
-                    if each.requirement.find(word) == -1:
-                        VacancyManager.del_vacancy_by_id(each.id)
+            for each in self._memory.values():
+                if each.info.description is not None and word in each.info.description:
+                    self.del_vacancy(each.id)
 
-            elif field == 'responsibility':
-                for each in VacancyManager.memory:
-                    if each.responsibility.find(word) == -1:
-                        VacancyManager.del_vacancy_by_id(each.id)
+                if each.info.short is not None and word in each.info.short:
+                    self.del_vacancy(each.id)
 
-    @staticmethod
-    def total():
-        resp.save(f'Общее количество вакансий в выборке: {len(VacancyManager.memory)}', level=2)
+    def total(self):
+        resp.save(f'Общее количество вакансий в выборке: {len(self._memory)}', level=2)
 
-    @staticmethod
-    def generate_short_html():
-        html = hh_html.short_html(VacancyManager.memory)
+    def generate_short_html(self):
+        """
+        Генерация упрощённого HTML списка
+        """
+        html = hh_html.short_html(self._memory)
 
         with open('output.html', mode='w', encoding='utf-8') as file:
             for line in html:
