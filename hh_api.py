cabe8f7d8840edcac227284a55fef1e989d478c9
Igor Zyktin <nicord@yandex.ru>
2018-11-14 12:43:59 +0300

    локальная загрузка

--- hh_api.py
+++ hh_api.py
@@ -1,64 +1,96 @@
 import json
 import requests
-from colorama import init, Fore
-init(autoreset=True)
+import resp
+
+LOCAL_MODE = True
 
 
-def _ask_hh_api(parameters):
+def get_data(keyword: str, area: int):
+    """
+    Обработчик обращений к данным
+    """
+    if LOCAL_MODE:
+        return load_local_vacancies(keyword, area)
+    else:
+        return load_remote_vacancies(keyword, area)
+
+
+def _request(method, parameters=None):
     """
     Отправка запроса к API HH.ru
     """
-    request = requests.get('https://api.hh.ru/vacancies/', parameters)
+    resp.save(f'Обращение к API HH. parameters = {parameters}', level=1)
+
+    if parameters:
+        request = requests.get(method, parameters)
+    else:
+        request = requests.get(method)
+
     status_code = request.status_code
-    request_dict = request.json()
 
     # нормальный режим
     if status_code == 200:
-        return request_dict
+        return request.json()
 
     elif status_code == 400:
-        print(Fore.RED + f'Ошибка 400: Некорректный запрос.')
-        argument = request_dict.get("bad_argument")
+        resp.save(f'Ошибка 400: Некорректный запрос. parameters = {parameters}', level=3)
+        argument = request.json().get("bad_argument")
         if argument:
-            print(Fore.RED + 'Неправильный аргумент:' + str(argument))
+            resp.save('Неправильный аргумент:' + str(argument), level=3)
 
     elif status_code == 403:
-        print(Fore.RED + 'Ошибка 403: Ошибка прав доступа.')
+        resp.save('Ошибка 403: Не хватает прав доступа', level=3)
     elif status_code == 404:
-        print(Fore.RED + 'Ошибка 404: Страница не найдена.')
+        resp.save('Ошибка 404: Страница не найдена.', level=3)
     elif status_code == 429:
-        print(Fore.RED + 'Ошибка 429: Превышен лимит просмотров.')
+        resp.save('Ошибка 429: Превышен лимит просмотров', level=3)
     elif status_code == 500:
-        print(Fore.RED + 'Ошибка 500: Нет доступа.')
+        resp.save('Ошибка 500: Нет доступа', level=3)
     elif status_code == 503:
-        print(Fore.RED + 'Ошибка 503: Нет доступа.')
+        resp.save('Ошибка 503: Нет доступа.', level=3)
     return {}
 
 
-def retrieve_vacancies(keyword, local=True):
+def load_remote_vacancies(keyword: str = 'python', area: int = 1):
     """
-    Скачивание данных из базы HH.ru
-    Ответ приходит в виде json-файла, в котором нас интересует поле items
+    Поисковый запрос на сайте HH.ru
     """
+    resp.save(f'Загрузка данных из интернета по запросу {keyword} в зоне {area}', level=1)
+
+    # по идее должны скачиваться файлы на страницах [0, pages)
+    # но данные последней никак не получается достать (возможно ошибка API)
+    per_page = 50   # Вакансий в странице
+
+    parameters = {'text': keyword, 'area': area, 'per_page': per_page, 'page': 0}
+    request = _request('https://api.hh.ru/vacancies/', parameters)
+
     storage = []
-    if local:
-        # имитация загрузки
-        with open('python.json', mode='r', encoding='utf-8-sig') as file:
-            storage = json.load(file)
-    else:
-        # по идее должны счкачиваться файлы на страницах [0, pages)
-        # но данные последней никак не получается достать (возможно ошибка API)
-        area = 1        # Москва
-        per_page = 50   # Вакансий в странице
+    storage.extend(request.get('items'))
+    pages = request.get('pages', 0)
 
-        parameters = {'text': keyword, 'area': area, 'per_page': per_page, 'page': 0}
-        request = _ask_hh_api(parameters)
-
-        storage.extend(request.get('items'))
-        pages = request.get('pages', 0)
-        for page in range(pages):
-            parameters = {'text': keyword, 'area': area, 'per_page': per_page, 'page': page}
-            request = _ask_hh_api(parameters)
-            storage.extend(request.get('items', []))
+    for page in range(pages):
+        parameters = {'text': keyword, 'area': area, 'per_page': per_page, 'page': page}
+        request = _request('https://api.hh.ru/vacancies/', parameters)
+        storage.extend(request.get('items', []))
 
     return storage
+
+
+def load_local_vacancies(keyword: str = 'python', area: int = 1):
+    """
+    Загрузка данных из локальной базы
+    """
+    resp.save(f'Загрузка данных из локальной базы по запросу {keyword} в зоне {area}', level=1)
+    with open('db\python.json', mode='r', encoding='utf-8') as file:
+        result = json.load(file)
+    return result
+
+
+def load_vacancy(vacancy_id):
+    """
+    Загрузка подробных данных о конкретной вакансии с сайта
+    """
+    resp.save(f'Запрос подробного описания вакансии {vacancy_id} с сайта hh.ru', level=2)
+    result = _request(method=f'https://api.hh.ru/vacancies/{vacancy_id}')
+    return result
+
