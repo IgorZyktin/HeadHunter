b73e85f48a8c1ba4e59700b434af3218535cf2b4
Igor Zyktin <nicord@yandex.ru>
2018-11-19 19:36:16 +0300

    адаптировано под статичные атрибуты

--- hh_html.py
+++ hh_html.py
@@ -9,10 +9,11 @@ import operator
 
 def short_html(vacancies):
     """
-    Генерация файла с корткими описаниями вакансий
+    Генерация файла с короткими описаниями вакансий
     """
     html = list()
-    header = """
+
+    header1 = """
     <!DOCTYPE html>
     <html>
     <link href="headhunter.css" rel="stylesheet">
@@ -37,26 +38,30 @@ def short_html(vacancies):
     </div>
     """.format(len(vacancies))
 
-    html.extend(header)
+    html.extend(header1)
     html.extend(header2)
 
-    for vacancy in sorted(vacancies.values(), key=operator.attrgetter('salary.avg_salary')):
-        if vacancy.info.description:
-            text = vacancy.info.description
+    for vacancy in sorted(vacancies.values(), key=operator.attrgetter('attr_06_salary_avg_')):
+
+        if vacancy.attr_10_description:
+            text = vacancy.attr_10_description
         else:
-            text = vacancy.info.short
+            text = vacancy.attr_11_short_descr
 
-        salary = vacancy.salary.str_salary
+        vac_id = vacancy.attr_01_id__
+        url = vacancy.attr_03_url_
+        name = vacancy.attr_02_name
+        salary = vacancy.attr_07_salary_str_
 
-        info = f""" 
+        info = """ 
         <div class="vacancy_short">
-        <button class="button button1" onclick="openbox({vacancy.id}); return false">Подробности</button>
-        <b>{vacancy.id}</b>: {salary} <a href="{vacancy.url}">{vacancy.name}</a>
-        <div id="{vacancy.id}" style="display:none;" class="descr">
-        {text}
+        <button class="button button1" onclick="openbox({0}); return false">Подробности</button>
+        <b>{0}</b>: {3} <a href="{1}">{2}</a>
+        <div id="{0}" style="display:none;" class="descr">
+        {4}
         <br>
         </div></div>
-        """
+        """.format(vac_id, url, name, salary, text)
         html.extend(info)
     html.append('</body>\n')
     html.append('</html>\n')
