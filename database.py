4383747ade56b035881616c9b1a0501f86f9d618
Igor Zyktin <nicord@yandex.ru>
2018-11-19 19:38:05 +0300

    sqlite

--- database.py
+++ database.py
@@ -5,6 +5,8 @@
 """
 import sqlite3
 from sqlite3 import Error
+import vacancy as Vacancy
+import responds
 
 
 def create_connection(db_file=r"DB\headhunter.db"):
@@ -30,57 +32,29 @@ def dump(vacancies):
 
     for vacancy in vacancies.values():
         print(vacancy)
-        db_data.append((vacancy.id,
-                        vacancy.name,
-                        vacancy.url,
-                        vacancy.description,
-                        vacancy.experience,
-                        vacancy.key_skills,
-                        vacancy.short,
-                        vacancy.published_at,
-                        vacancy.created_at,
-                        vacancy.edited_at,
-                        vacancy.salary_from,
-                        vacancy.salary_to,
-                        vacancy.avg_salary,
-                        vacancy.str_salary,
-                        vacancy.city,
-                        vacancy.street,
-                        vacancy.employer_id,
-                        vacancy.employer_name,
-                        vacancy.employer_url,
-                        vacancy.employer_logo
-                        ))
+        db_data.append([*vacancy.attribute_names()])
 
+    names = ','.join(Vacancy.Vacancy.attribute_names())
     conn = create_connection()
     with conn:
         cursor = conn.cursor()
-        cursor.execute("""CREATE TABLE IF NOT EXISTS main (id integer, 
-                                                        name text,
-                                                        url text,
-                                                        description text,
-                                                        experience text,
-                                                        key_skills text,
-                                                        short text,
-                                                        published_at float, 
-                                                        created_at float,
-                                                        edited_at float,
-                                                        salary_from integer,
-                                                        salary_to integer,
-                                                        avg_salary integer,
-                                                        str_salary text,
-                                                        city text,
-                                                        street text,
-                                                        employer_id integer,
-                                                        employer_name text,
-                                                        employer_url text,
-                                                        employer_logo text
-                                                        )""")
+        cursor.execute("""CREATE TABLE IF NOT EXISTS main (?)""", (names,))
         for line in db_data:
             cursor.executemany("INSERT INTO main VALUES (?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?)", (line,))
         conn.commit()
         return
 
+
+def drop_db():
+    """
+    Удаление таблицы
+    """
+    responds.save(f'Удаление таблицы базы данных', level=5)
+    conn = create_connection()
+    # with conn:
+        # cursor = conn.cursor()
+        # cursor.execute("""DROP TABLE address""")
+        # return
 
 
 def main():
