f62ec72451c0ff1b5ac38dff28cb5a381a50897e
Igor Zyktin <nicord@yandex.ru>
2018-11-19 19:38:36 +0300

    форматирование

--- responds.py
+++ responds.py
@@ -16,11 +16,6 @@ logging.basicConfig(
 )
 
 LEVEL = 1
-# 1 - Самое подробное описание
-# 2 - Незначительные события
-# 3 - Важные события
-# 4 - Очень важные события
-# 5 - Критический сбой
 
 
 def save(text, color=None, level: int = 1):
@@ -28,22 +23,27 @@ def save(text, color=None, level: int = 1):
     Вывод всех данных скрипта собран в этой функции
     """
     if level == 1:
+        # 1 - Самое подробное описание
         resulting_color = Fore.GREEN
         logging.info(text)
 
     elif level == 2:
+        # 2 - Незначительные события
         resulting_color = Fore.LIGHTGREEN_EX
         logging.info(text)
 
     elif level == 3:
+        # 3 - Важные события
         resulting_color = Fore.YELLOW
         logging.warning(text)
 
     elif level == 4:
+        # 4 - Очень важные события
         resulting_color = Fore.LIGHTRED_EX
         logging.error(text)
 
     elif level == 5:
+        # 5 - Критический сбой
         resulting_color = Fore.RED
         logging.critical(text)
 
