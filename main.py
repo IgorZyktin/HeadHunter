72055c627afd4479f1b9b793b92377d0124ffb0f
Igor Zyktin <nicord@yandex.ru>
2019-01-08 23:03:11 +0300

    сделан более красивый вывод текста при обработке

--- main.py
+++ main.py
@@ -4,64 +4,73 @@
     Основной модуль программы
 
 """
-from handlers import tokenize, is_int
-from vacancy import VacancyManager
 from colorama import Fore, init
+
+from hh_handlers import tokenize, is_int
+from hh_vacancy import VacancyManager
 init(autoreset=True)
+
+
+def greetings():
+    """
+        Вывод стартового текста
+    """
+    print()
+    print(Fore.LIGHTBLUE_EX + ' HeadHunter '.center(79, '~'))
+    print(Fore.LIGHTBLUE_EX + ' Поисковый робот '.center(79, '~'))
+    print()
+    print('\tПримеры запросов:')
+    print('\t' + Fore.YELLOW + '> Сварщик')
+    print('\t' + Fore.YELLOW + '> Сварщик, Тверь')
+    print('\t' + Fore.YELLOW + '> Сварщик AND аргон, Тверь')
+    print('\t' + Fore.YELLOW + '> Сварщик and аргон not мангал, Тверь')
+    print('\t' + Fore.YELLOW + '> Сварщик AND аргон NOT мангал, Тверь')
 
 
 def main():
     """
         Главная функция
     """
-    print()
-    print(Fore.LIGHTBLUE_EX + ' HeadHunter '.center(79, '~'))
-    print(Fore.LIGHTBLUE_EX + 'Поисковый робот'.center(79, ' '))
-    print()
-    print('\tПримеры запросов:')
-    print('\t\t' + Fore.YELLOW + 'Сварщик')
-    print('\t\t' + Fore.YELLOW + 'Сварщик,', Fore.LIGHTMAGENTA_EX + 'Тверь')
-    print('\t\t' + Fore.YELLOW + 'Сварщик' + Fore.GREEN + ' AND ' + Fore.BLUE + 'нержавейка, '
-          + Fore.LIGHTMAGENTA_EX + 'Тверь')
-    print('\t\t' + Fore.YELLOW + 'Сварщик' + Fore.GREEN + ' AND ' + Fore.BLUE + 'нержавейка'
-          + Fore.RED + ' NOT ' + Fore.BLUE + 'мангалы, ' + Fore.LIGHTMAGENTA_EX + 'Тверь')
-
-    print()
-    print('\tВведите поисковый запрос:')
+    greetings()
     while True:
-        user_input = input('\t>:')
+        print()
+        print('\tВведите поисковый запрос:')
+
+        user_input = input('\t> ')
+        # user_input = 'Сварщик AND аргон NOT мангал, Тверь'
+
         if not user_input:
             continue
 
-        # user_input = 'Сварщик AND нержавейка NOT мангалы, Тверь'
-        print('\tПолучено:', user_input)
-
+        print('\tПолучено:', Fore.LIGHTRED_EX + user_input)
         print()
 
         request, with_this, without_this, city_name, city_code = tokenize(user_input)
 
-        print('\t    Основной запрос:', Fore.YELLOW + request)
+        print('\tОсновной запрос:', Fore.YELLOW + request)
 
         if with_this:
-            print('\t    Что должны быть:', Fore.BLUE + ', '.join(with_this))
+            print('\t    Должно быть:', Fore.BLUE + ', '.join(with_this))
 
         if without_this:
-            print('\tЧего не должно быть:', Fore.RED + ', '.join(without_this))
+            print('\t Не должно быть:', Fore.RED + ', '.join(without_this))
 
-        print('\t     В каком городе:', city_name.title(), f'(код {city_code})')
+        print('\t В каком городе:', city_name.title(), f'(код {city_code})')
 
         print()
         print('\tЗапрос распознан правильно? 1 - да, 0 - нет')
-        user_input = input('\t>:')
+
+        user_agreement = input('\t> ')
+
         print()
-
-        if not is_int(user_input):
+        if not is_int(user_agreement):
             continue
 
-        if int(user_input) == 0:
+        if int(user_agreement) == 0:
             continue
 
-        manager = VacancyManager(keyword=request, area=1)
+        print('\tОтправляем запрос на сервер hh.ru...')
+        manager = VacancyManager(keyword=user_input, area=city_code)
 
         if with_this:
             manager.purge_without(with_this)
@@ -70,12 +79,23 @@ def main():
             manager.purge_with(without_this)
 
         manager.demonstrate()
-        manager.generate_html()
+        if manager.total() == 0:
+            continue
+
+        print('\tЗапросить подробности по найденным вакансиям? 1 - да, 0 - нет')
+        user_agreement = input('\t> ')
+        print()
+
+        if not is_int(user_agreement):
+            continue
+
+        if int(user_agreement) == 0:
+            continue
+
+        manager.detail_all()
+        manager.generate_results()
         manager.erase_memory()
         del manager
-
-        print()
-        print('\tВведите новый поисковый запрос:')
 
 
 if __name__ == '__main__':
